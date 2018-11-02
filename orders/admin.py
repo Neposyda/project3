6e9a0ab6a685f5ab681a16c6cd64409f95b213ca
Neposyda <if270974svm@gmail.com>
2018-11-02 19:27:13 +0200

    create/filling Models in models.py (at work)

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categorie, Dish, Price, PriceCategories
+
+admin.site.register(Categorie)
+admin.site.register(Dish)
+admin.site.register(Price)
+admin.site.register(PriceCategories)
--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Categorie(models.Model):
     name = models.CharField(max_length=15, blank=False)
+    subtype=models.IntegerField(default=0, blank=False)
 
     def __str__(self):
         return f"{self.name}"
