fe14912a92e8ca5e7a525fc88e4db5f2a79b435d
Neposyda <if270974svm@gmail.com>
2018-12-10 18:13:36 +0200

    realiz. copy/del item in div order (at work)

--- orders/static/index.js
+++ orders/static/index.js
@@ -1,6 +1,7 @@
 let ctgDishAll, dishAll, ctgPriceAll, priceAll;
 let ctgInOrder={};
 
+
 function createNewItem (dishId){
     let ctgId=dishAll.getIdCategorie(dishId);
     let ctgName=ctgDishAll.getName(ctgId);
@@ -22,52 +23,50 @@ function createNewItem (dishId){
     //create elements of new item in order
     const divNewItem=document.createElement('div');
     divNewItem.setAttribute('class','item');
-    divNewItem.setAttribute('id','item'+dishId);
+    // divNewItem.setAttribute('id','item'+dishId); // !!!!!!
 
    //create elements of new item in order
-    const divName= document.createElement('input','name'+dishId);
+    const divName= document.createElement('input');{
     divName.setAttribute('type','text');
-    divName.setAttribute('class', 'itemLine');
+    divName.setAttribute('class', 'itemLine name');
     divName.setAttribute('value',dishAll.getName(dishId));
-    divName.setAttribute('disabled','off');
+    divName.setAttribute('disabled','off');}
 
-    const divType=document.createElement('input');
+    const divType=document.createElement('input');{
     divType.setAttribute('type','button');
     divType.setAttribute('class', 'itemLine btntype');
-    divType.setAttribute('value','Small');
-    // divType.setAttribute('value','0');
+    divType.setAttribute('value','Small');}
 
-    const divCount=document.createElement('input');
+    const divCount=document.createElement('input');{
     divCount.setAttribute('type','number');
     divCount.setAttribute('class','itemLine count');
     divCount.setAttribute('value','0');
     divCount.setAttribute('min','0');
     divCount.setAttribute('max','10');
-    divCount.setAttribute('step','1');
+    divCount.setAttribute('step','1');}
 
-    const divPrice=document.createElement('input');
+    const divPrice=document.createElement('input');{
     divPrice.setAttribute('type','text');
     divPrice.setAttribute('class','itemLine price');
     divPrice.setAttribute('value', '$ <price>');
-    divPrice.setAttribute('disabled','off');
-    // divPrice.setAttribute('max-width','50px');
+    divPrice.setAttribute('disabled','off');}
 
-    const divSumm=document.createElement('input');
+    const divSumm=document.createElement('input');{
     divSumm.setAttribute('type','text');
     divSumm.setAttribute('class','itemLine summ');
-    divSumm.setAttribute('value', '$ <summ>');
-    divSumm.setAttribute('disabled','off');
-    // divSumm.setAttribute('max-width','100px');
+    divSumm.setAttribute('value', '$ 0.00>');
+    divSumm.setAttribute('disabled','off');}
 
-    const divCopy=document.createElement('input');
+    const divCopy=document.createElement('input');{
     divCopy.setAttribute('type','button');
     divCopy.setAttribute('class','itemLine btncopy');
-    divCopy.setAttribute('value','+');
+    divCopy.setAttribute('value','+');}
 
-    const divDel=document.createElement('input');
+    const divDel=document.createElement('input');{
     divDel.setAttribute('type','button');
     divDel.setAttribute('class','itemLine btndel');
-    divDel.setAttribute('value','-');
+    divDel.setAttribute('value','-');}
+
 
     divNewItem.appendChild(divName);
     divNewItem.appendChild(divType);
@@ -79,9 +78,8 @@ function createNewItem (dishId){
 
     divCtgr.appendChild(divNewItem);
 }
-    // alert(ctgDishAll.getName(dishAll.getIdCategorie(dishId)))}
 
-    document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
         // Open new request to get new posts.
         const request = new XMLHttpRequest();
         request.open('GET', '/data');
@@ -107,27 +105,50 @@ function createNewItem (dishId){
                 return dishAll[i-1].categorie_id;
             };
 
-            ctgPriceAll=dataJSON['categories_price_list'];
-            priceAll=dataJSON['prices_list'];
+            // ctgPriceAll=dataJSON['categories_price_list'];
+            // priceAll=dataJSON['prices_list'];
+            // priceAll.getPrice=(dishId, priceCtg=0,  type='small')=>{
+            //     let i=0;
+            //     while (priceAll[i].id!=parseInt(dishId) && priceAll[i].categorie_id==parseInt(priceCtg)){++i;}
+            //     console.log(priceAll[i-1][type]);
+            // }
         };
 
-    document.querySelector('#dishes_list').onclick = (e)=>{
+        //event listener for order
+        document.querySelector('.order').onclick=(e)=>{
+            obj=e.target;
+            if (obj.type=='button') {
+                switch (obj.classList[1]) {
+                    case 'btntype':
+                        obj.value=(obj.value=='Small')?'Large':'Small';
+                        break;
+                    case 'btncopy':
+                        const groupItem=obj.parentElement.parentElement;
+                        const cloneItem=obj.parentElement.cloneNode(true);
+                        // cloneItem.childNodes
+                        groupItem.appendChild(cloneItem);
+                        break;
+                    case 'btndel':
+                        obj.parentElement.remove();
+                        break;
+                }
+            }
+        };
+
+        document.querySelector('#dishes_list').onclick = (e)=>{
         //click group name
         if (e.target.className=='ctgr_name') {
             const v = document.querySelector("ul > ."+e.target.id);
             v.hidden= !v.hidden;
-        };
+        }
         //select dish
         if (e.target.className=="dish"){
             createNewItem(e.target.id);
-            e.target.setAttribute('class','select');  //className=e.target.className + ' select';
-            // createNewItemElement(e.target.innerText);
+            e.target.classList.add('select')
         }
-    };
+        };
 
-    document.querySelector('#title_order').onclick=()=>{
-        document.querySelector('.order').hidden=!document.querySelector('.order').hidden;
-    };
-
-    // document.querySelector('')
+        document.querySelector('#title_order').onclick=()=>{
+            document.querySelector('.order').hidden=!document.querySelector('.order').hidden;
+        };
 });
